03e23e07494bb4376fc6de1344e8d430d1ae34a6
lewys-miugo <lewiswambugu01@gmail.com>
2023-01-22 15:10:18 +0300

    adding the contacts and form submmit functionality

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -62,42 +62,46 @@
 <footer aria-label="Site Footer" id="footer" class="bg-white">
     <div class="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
         <div class="lg:grid lg:grid-cols-2">
-            <div
-                class="py-8 border-b border-gray-100 lg:order-last lg:border-b-0 lg:border-l lg:py-16 lg:pl-16"
-            >
-                <div class="block text-teal-600 lg:hidden">
-
-                </div>
+            <div class="py-8 border-b border-gray-100 lg:order-last lg:border-b-0 lg:border-l lg:py-16 lg:pl-16">
+                
 
                 <div class="mt-8 space-y-4 lg:mt-0">
                     <span class="hidden w-10 h-1 bg-indigo-600 rounded lg:block"></span>
-
                     <div>
-                        <h2 class="text-2xl font-medium text-gray-900">Email Me</h2>
-
-                        <textarea name="" id="" class="block bg-gray-100 p-1 outline-blue-300 w-full"></textarea>
+                        <h2 class="text-2xl font-medium text-gray-900">Contact me via Email</h2>
                     </div>
+                    <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md">
+                        
+                      
+                        
+                        <form target="_blank" 
 
-                    <form class="w-full mt-6">
-                        <label for="UserEmail" class="sr-only"> Email </label>
-
-                        <div
-                            class="p-2 border border-gray-100 rounded-md focus-within:ring sm:flex sm:items-center sm:gap-4"
-                        >
-                            <input
-                              type="email"
-                              id="UserEmail"
-                              placeholder="john@rhcp.com"
-                              class="w-full border-none focus:border-transparent focus:ring-transparent sm:text-sm"
-                            />
-
-                            <button
-                                class="w-full px-6 py-3 mt-1 text-sm font-bold tracking-wide text-white uppercase transition-none bg-indigo-600 rounded hover:bg-teal-600 sm:mt-0 sm:w-auto sm:flex-shrink-0"
-                            >
-                                Send
-                            </button>
-                        </div>
-                    </form>
+                          action="https://formsubmit.co/mogutu005@gmail.com" 
+                          
+                          method="POST">
+                          <div class="form-group mb-6">
+                            <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="name" name="name"
+                              placeholder="Name">
+                          </div>
+                          <div class="form-group mb-6">
+                            <input type="email" name="email" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+                              placeholder="Email address" required>
+                          </div>
+                          <div class="form-group mb-6">
+                            <textarea name="message"
+                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700
+                              bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleFormControlTextarea13"
+                            rows="3"
+                            placeholder="Message"
+                          ></textarea>
+                          </div>
+                          <button type="submit" class="
+                            w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg
+                            focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                            active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Send</button>
+                        </form>
+                        
+                      </div>
                 </div>
             </div>
 
@@ -107,7 +111,6 @@
                 <a href="#">
                     <img class="h-10 inline"
                          src="https://imgs.search.brave.com/0AQQolYpCoid6wIJGQ4JKqUsQOaBz8a2CSC0am-Quok/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5l/bjJOZVZ2LWFTQ09l/Qm84MFdXTVB3SGFI/YSZwaWQ9QXBp">
-                        Ogutu
                 </a>
             </span>
                 </div>
@@ -122,15 +125,15 @@
                             <ul class="space-y-4 text-sm">
                                 <li><a href="{{url('/')}}" class="text-gray-700 transition hover:opacity-75">Home</a>
                                 </li>
-                                <li><a href="{{url('about')}}"
+                                <li><a href="{{url('/about')}}"
                                        class="text-gray-700 transition hover:opacity-75">About</a></li>
-                                <li><a href="{{url('reflections')}}" class="text-gray-700 transition hover:opacity-75">Reflections</a>
+                                <li><a href="{{url('/reflections')}}" class="text-gray-700 transition hover:opacity-75">Reflections</a>
                                 </li>
-                                <li><a href="{{url('publications')}}" class="text-gray-700 transition hover:opacity-75">Publications</a>
+                                <li><a href="{{url('/publications')}}" class="text-gray-700 transition hover:opacity-75">Publications</a>
                                 </li>
-                                <li><a href="{{url('currentinterests')}}"
+                                <li><a href="{{url('/currentinterests')}}"
                                        class="text-gray-700 transition hover:opacity-75">Current Interest</a></li>
-                                <li><a href="{{url('gallery')}}" class="text-gray-700 transition hover:opacity-75">Gallery</a>
+                                <li><a href="{{url('/gallery')}}" class="text-gray-700 transition hover:opacity-75">Gallery</a>
                                 </li>
                                 <li><a href="/admin" class="text-gray-700 transition hover:opacity-75">Dashboard</a>
                                 </li>
@@ -138,33 +141,7 @@
                         </nav>
                     </div>
 
-                    {{--<!-- <div>
-                        <p class="font-medium text-gray-900">Publications & Reflections</p>
-                        <div><span class="hidden w-13 h-1 bg-indigo-600 rounded lg:block"></span></div>
-
-
-                        <nav aria-label="Footer Navigation - Company" class="mt-6">
-                            <ul class="space-y-4 text-sm">
-
-                                <li>
-                                    <a href="#" class="text-gray-700 transition hover:opacity-75">
-                                          <p>lorem 3</p>
-                                    </a>
-                                </li>
-
-                                
-
-                                <li>
-                                  @foreach($externalpublications->slice(0,3) as $externalpublication)
-                                  
-                                    <a href="#" class="text-gray-700 transition hover:opacity-75">
-                                        <p>{{$externalpublication->title}}</p>
-                                    </a>
-                                  @endforeach  
-                                </li>
-                            </ul>
-                        </nav>
-                    </div> --> --}}
+                   
 
                     <div>
                         <p class="font-medium text-gray-900">Contacts</p>
@@ -175,9 +152,16 @@
                                 @foreach($contact as $contact)
                                     <li>
                                       @if($contact->type=="social")
-                                        <a href="{{$contact->link}}" class="text-gray-700 transition hover:opacity-75">
-                                            
+                                        <a href="{{$contact->value}}" class="text-gray-700 transition hover:opacity-75">{{$contact->name}}
                                         </a>
+
+                                      @endif
+                                    </li>
+                                    <li>
+                                      @if($contact->type=="phone_number")
+                                        <a href="{{$contact->value}}" class="text-gray-700 transition hover:opacity-75">{{$contact->name}}
+                                        </a>
+
                                       @endif
                                     </li>
                                 @endforeach
