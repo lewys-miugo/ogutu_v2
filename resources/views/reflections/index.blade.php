b2ee231b744f2d5799e83ec03b6ebfcf710b56ac
lewys-miugo <lewiswambugu01@gmail.com>
2023-01-26 08:20:36 +0300

    Making the card's a tag h-auto

--- resources/views/reflections/index.blade.php
+++ resources/views/reflections/index.blade.php
@@ -36,7 +36,7 @@
         <div class="flex flex-col mt-3 md:flex-row md:flex-wrap justify-center items-center">
             @foreach($internalreflections as $internalreflection)
                 <a target="_blank" href="{{route('reflections.show', $internalreflection->id)}}"
-                   class="transition duration-200 ease-out transform hover:scale-105 my-2 w-5/6 h-64 rounded shadow-2xl md:w-2/5 lg:w-2/5 mx-2">
+                   class="transition duration-200 ease-out transform hover:scale-105 my-2 w-5/6 h-auto rounded shadow-2xl md:w-2/5 lg:w-2/5 mx-2">
                     <img
                         class="w-full h-4/5 object-cover rounded-t"
                         src={{ asset("storage/{$internalreflection->image}") }} alt="">
@@ -53,19 +53,19 @@
 
         <div class="flex flex-col mt-3 md:flex-row md:flex-wrap justify-center items-center">
             @foreach($externalreflections as $externalreflection)
-                <a target="_blank" href="{{$externalreflection->link}}"
-                   class="transition duration-200 ease-out transform hover:scale-105 my-2 w-5/6 h-64 rounded shadow-2xl md:w-2/5 lg:w-2/5 mx-2">
-                    <img
-                        class="w-full h-4/5 object-cover rounded-t"
-                        src={{ asset("storage/{$externalreflection->image}") }} alt="">
-                    <div class="flex justify-between items-center mr-2">
-                        <div class="flex flex-col">
-                            <time class="text-xs pl-2 text-gray-500">{{$externalreflection->time}}</time>
-                            <p class="p-2 font-bold">{{ $externalreflection->title }}</p>
+                    <a target="_blank" href="{{$externalreflection->link}}"
+                    class="transition duration-200 ease-out transform hover:scale-105 my-2 w-5/6 h-auto rounded shadow-2xl md:w-2/5 lg:w-2/5 mx-2">
+                        <img
+                            class="w-full h-4/5 object-cover rounded-t"
+                            src={{ asset("storage/{$externalreflection->image}") }} alt="">
+                        <div class="flex justify-between items-center mr-2">
+                            <div class="flex flex-col">
+                                <time class="text-xs pl-2 text-gray-500">{{$externalreflection->time}}</time>
+                                <p class="p-2 font-bold ">{{ $externalreflection->title }}</p>
+                            </div>
+                            <p class="rounded-3xl text-white bg-red-600 px-2">{{ $externalreflection->tag }}</p>
                         </div>
-                        <p class="rounded-3xl text-white bg-red-600 px-2">{{ $externalreflection->tag }}</p>
-                    </div>
-                </a>
+                    </a>
             @endforeach
         </div>
     </div>
