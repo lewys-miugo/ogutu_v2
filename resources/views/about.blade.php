3e1def74d0a4094cf697757edbbba761e895e426
lewys-miugo <lewiswambugu01@gmail.com>
2023-04-14 14:26:42 +0300

    Removing the old components

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,24 +3,26 @@
 @extends('layout.layout')
 
 @section('content')
+
+
+<!-- OLD componet -->
 <!-- component -->
-    <div class=" h-1/2 lg:h-screen bg-white relative md:z-10">
+    <!-- <div class=" h-1/2 lg:h-screen bg-white relative md:z-10">
         <img class="absolute bottom-0 right-0 lg:left-0 mx-auto h-full shadow-xl w-full object-cover"
              src="{{url('/images/i8.jpeg')}}"
              alt="">
 
-        <!-- Animated text -->  
         <div
             class="absolute top-1/3 left-5 text-xl sm:left-10 sm:text-4xl md:left-1/4 md:text-6xl lg:left-5 xl:left-38 xl:text-7xl font-bold">
             <span class="text-white">More</span>
             <p class="text-red-600">About</p>
             <p class="text-white">Me</p>
         </div>
-    </div>
+    </div> -->
 <!-- New trial section-->
 
     <!-- New trial layout -->
-    <div class="bg-white pt-6">
+    <!-- <div class="bg-white pt-6">
         <div class="">
             <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-12">
                 <div class="col-span-3 flex flex-col justify-center items-center">
@@ -62,12 +64,11 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
 
-    <div class="bg-white">
+    <!-- <div class="bg-white">
         <div id="experience" class="py-10 flex flex-col lg:flex-row item-center gap-20 bg-white">
-                <!-- Left -->
                 <div class="lg:w-2/3 px-7 mx-3 my-auto flex flex-col gap-3 ">
                     <h1 class="text-indigo-600 text-center font-bold text-2xl">About Me</h1>
 
@@ -76,8 +77,7 @@
                     </p>
                 </div>
               
-                <!-- <div class="my-auto flex justify-center item-center lg:w-1/3">                    
-                </div> -->
+                </div> 
                 <div class="p-5 lg:p-3 lg:w-1/3 flex flex-wrap justify-between">
                         @foreach($gallery->slice(0,2) as $gallery)
                             <div class="w-full shadow-xl rounded-lg p-5 my-3 md:my-3 flex flex-col gap-2">
@@ -93,7 +93,7 @@
             </div>
 
         </div>
-    </div>
+    </div> -->
 @endsection
 <body>
 </body>
