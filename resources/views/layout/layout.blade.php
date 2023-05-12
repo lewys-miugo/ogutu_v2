5b683f06c812346012241f1c4a399a5160b11722
lewys-miugo <lewiswambugu01@gmail.com>
2023-05-12 22:23:12 +0300

    Separating vite line in layouts

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=, initial-scale=1.0">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    @vite('resources/css/app.css','resources/js/app.js')
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
     <!-- <title>Moses Ogutu</title> -->
     <title>{{ config('app.name','Moses Ogutu')}} - @yield('title')</title>
 </head>
