9683236ba20c38c128a32026eacad1afd37b299d
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-11 01:30:01 +0300

    creating academia page

---
+++ app/Http/Livewire/AcademiaComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AcademiaComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.academia-component');
+    }
+}
