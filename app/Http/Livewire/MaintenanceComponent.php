6d42784204f190a9b308ea7bf752b77a5265bbbb
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-11 15:56:29 +0300

    Adding maintenance pages

---
+++ app/Http/Livewire/MaintenanceComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MaintenanceComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.maintenance-component');
+    }
+}
---
+++ resources/views/livewire/maintenance-component.blade.php
@@ -0,0 +1,26 @@
+<div>
+    <!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Maintenance</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+</head> -->
+
+<div class="bg-gray-200 flex items-center justify-center h-screen my-8">
+    <div class="max-w-md mx-auto p-8 bg-white rounded shadow box-shadow-xl">
+        <h1 class="text-4xl text-blue-400 font-bold mb-4">Site Maintenance</h1>
+        <p class="text-gray-700 mb-8">We are currently performing maintenance on the site. Please check back later.</p>
+        <div class="flex justify-center">
+            <p  class="bg-blue-500 hover:bg-blue-600 text-white text-center font-bold py-2 px-4 rounded">
+                Contact me through my channels in the footer below.
+            </p>
+        </div>
+    </div>
+</div>
+
+<!-- </html> -->
+
+</div>
