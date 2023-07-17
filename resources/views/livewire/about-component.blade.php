f3b8fdbb93a987587eed7d8a13bb96d7ce699410
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-18 01:15:44 +0300

    Adding the about details

--- resources/views/livewire/about-component.blade.php
+++ resources/views/livewire/about-component.blade.php
@@ -1,3 +1,216 @@
 <div>
     {{-- The best athlete wants his opponent at his best. --}}
+    <section >
+    <!-- Banner -->
+    <div class="mx-2">
+        <div class="bg-bluetange float-left z-05 absolute rounded-b-lg ml-16"> 
+            <div class="m-4">
+                <p class="text-white"><a class="hover:underline underline-offset-4" href="{{'/'}}">Home</a> <span> /</span> <a class="underline underline-offset-4" href="{{'about'}}"> About Moses Ogutu</a></p>
+            </div>
+        </div>
+        <div class="lg:absolute h-full w-full m-4 p-4">
+            <!-- Banner -->
+            
+            <div class="grid max-w-screen-4xl ml-12 mr-14 pt-8 mx-auto lg:gap-8 xl:gap-0 lg:pt-2 lg:grid-cols-12">
+                
+                <div class="mr-auto place-self-center lg:col-span-7">
+                    <div class="">
+                        <img src="/images/box_logo.png" class="mt-8 h-12 w-12" alt="">
+                        <div class="ml-12">
+                            <h1 class="max-w-2xl mb-4 text-bluetange text-xsitangeh font-extrabold tracking-tight leading-none ">Prof  Moses  Ogutu</h1>
+                            <p class="max-w-2xl mb-6 text-bluetange font-semibold lg:mb-8 md:text-lg lg:text-xl">I am a global technocrat and currently serving as Kenya’s Ambassador to Belgium. Previously, I was the Permanent Secretary of Kenya’s Ministry of Information and Communication.</p>
+                            <a href="{{'profile'}}" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-semibold text-center text-bluetange border border-gray-300 rounded-lg bg-orangelink hover:bg-gray-100 focus:ring-4 focus:ring-blue-500">
+                                Read Full Profile
+                            </a> 
+                            <a href="{{''}}" class="inline-flex items-center justify-center px-5 py-3 text-base font-semibold text-center text-bluetange border border-gray-300 rounded-lg bg-orangelink hover:bg-gray-100 focus:ring-4 focus:ring-blue-500">
+                                Download Printable Profile
+                            </a> 
+                        </div>
+                    </div>
+                </div>
+                <div class="h-48 xl:w-full xl:h-full lg:col-span-5 lg:flex">
+                    <img src="{{url('/images/profileimg-removebg-preview.png')}}" alt="mockup">
+                </div>                
+            </div>
+        </div>
+       
+        <img src="/images/background_pattern1.png" alt="" class=" w-full h-min invisible lg:visible">
+
+    </div>
+
+    
+    <!-- CARDS section -->
+    <div class="m-4 lg:ml-20">
+            <img src="/images/box_logo.png" class="h-14 w-14" alt="">
+            <h2 class="pb-4 text-sitangeh text-bluetange pl-14 font-extrabold ">Specific Sections</h2>
+    </div>
+    <div class='flex items-center flex-wrap  justify-center flex-col-2 mt-8 py-4 min-h-screen p-2 m-2 lg:mx-32 from-gray-700 via-gray-800 to-gray-900 bg-gradient-to-br'>
+        <!-- Card 1 -->
+        <div class="relative w-full group max-w-lg w-lg min-w-0 mx-auto mt-12 mb-12 break-words bg-white border shadow-2xl md:max-w-sm rounded-xl">
+            <div class="pb-6">
+                <div class="flex flex-wrap justify-center">
+                    <div class="flex justify-center w-full">
+                        <div class="relative">
+                            <img src="/images/no_tie_shot.jpg" class="dark:shadow-xl border-blue-400 rounded-full align-middle border-8 absolute -m-16 -ml-18 lg:-ml-16 max-w-[150px]" />
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-2 mt-20 pt-2 text-center">
+                    <h3 class="mb-1 font-bold leading-normal text-xxl text-bluetange">Profile</h3>
+                    <div class="flex flex-row justify-center w-full mx-auto space-x-2 text-center">
+                        
+                        <div class="text-base font-normal tracking-tight text-gray-600 font-mono ">
+                            <p>
+                                Moses Ogutu has over 20 years of business, finance and technology expertise gained…
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full text-center">
+                   <a href="{{'profile'}}" class="underline text-redlink font-semibold text-sm hover:text-orangelink">View More</a>                        
+                </div>
+            </div>   
+        </div>
+        <!-- Card2 -->
+        <div class="relative w-full group max-w-lg min-w-0 mx-auto mt-12 mb-12 break-words bg-white border shadow-2xl md:max-w-sm rounded-xl">
+            <div class="pb-6">
+                <div class="flex flex-wrap justify-center">
+                    <div class="flex justify-center w-full">
+                        <div class="relative">
+                            <img src="/images/profileimg-removebg-preview.png" class="dark:shadow-xl border-blue-400 rounded-full align-middle border-8 absolute -m-16 -ml-18 lg:-ml-16 max-w-[150px]" />
+                        </div>
+                    </div>
+                </div>
+                <div class="m-2 mt-20 pt-2 text-center">
+                    <h3 class="mb-1 font-bold leading-normal text-xxl text-bluetange">Key Experience</h3>
+                    <div class="flex flex-row justify-center w-full mx-auto space-x-2 text-center">
+
+                        <div class="text-base font-normal tracking-tight text-gray-600 font-mono ">
+                            <p>
+                                Moses Ogutu has over 20 years of business, finance and technology expertise gained…
+                            </p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="w-full text-center">
+                   <a href="#" class="underline text-redlink font-semibold text-sm hover:text-orangelink">View More</a>                        
+                </div>
+            </div>   
+        </div>
+        <!-- Card 3 -->
+        <div class="relative w-full group max-w-lg min-w-0 mx-auto mt-12 mb-12 break-words bg-white border shadow-2xl md:max-w-sm rounded-xl">
+            <div class="pb-6">
+                <div class="flex flex-wrap justify-center">
+                    <div class="flex justify-center w-full">
+                        <div class="relative">
+                            <img src="/images/tie_shot.jpg" class="dark:shadow-xl border-blue-400 rounded-full align-middle border-8 absolute -m-16 -ml-18 lg:-ml-16 max-w-[150px]" />
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-2 mt-20 pt-2 text-center">
+                    <h3 class="mb-1 font-bold leading-normal text-xxl text-bluetange">Membership & Honors</h3>
+                    <div class="flex flex-row justify-center w-full mx-auto space-x-2 text-center">
+                        <div class="text-base font-normal tracking-tight text-gray-600 font-mono ">
+                            <p>
+                                Moses Ogutu has over 20 years of business, finance and technology expertise gained…
+                            </p>
+                        </div>
+                        <!-- End: /typography/_h3.antlers.html -->
+                    </div>
+                    <div class="w-full text-center">
+                        <div class="flex justify-center pt-8 pb-0 lg:pt-4">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full text-center">
+                   <a href="#" class="underline text-redlink font-semibold text-sm hover:text-orangelink">View More</a>                        
+                </div>
+            </div>   
+        </div>
+        <!-- card 4 -->
+        <div class="relative w-full group max-w-lg min-w-0 mx-auto mt-12 mb-12 break-words bg-white border shadow-2xl md:max-w-sm rounded-xl">
+            <div class="pb-6">
+                <div class="flex flex-wrap justify-center">
+                    <div class="flex justify-center w-full">
+                        <div class="relative">
+                            <img src="/images/ruto2.jpeg" class="dark:shadow-xl border-blue-400 rounded-full align-middle border-8 absolute -m-16 -ml-18 lg:-ml-16 max-w-[150px]" />
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-2 mt-20 pt-2 text-center">
+                    <h3 class="mb-1 font-bold leading-normal text-xxl text-bluetange">Talks & Conferences</h3>
+                    <div class="flex flex-row justify-center w-full mx-auto space-x-2 text-center">
+                        <div class="text-base font-normal tracking-tight text-gray-600 font-mono ">
+                            <p>
+                                Moses Ogutu has over 20 years of business, finance and technology expertise gained…
+                            </p>
+                        </div>
+                    </div>
+                    <div class="w-full text-center">
+                        <div class="flex justify-center pt-8 pb-0 lg:pt-4">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full text-center">
+                   <a href="#" class="underline text-redlink font-semibold text-sm hover:text-orangelink">View More</a>                        
+                </div>
+            </div>   
+        </div>
+    </div>
+
+    <!-- ====== Gallery Section Start -->
+        
+    <section id="gallery" class="bg-white pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+
+        <div class="m-4 lg:ml-20">
+            <img src="/images/box_logo.png" class="h-14 w-14" alt="">
+            <h2 class="pb-4 text-sitangeh text-bluetange pl-14 font-extrabold ">Photo Gallery</h2>
+        </div>
+
+        <div class="container mx-auto">
+            <div class="m-4 flex flex-wrap">
+                <!-- 1st card -->
+                <div class="w-full px-4 md:w-1/2 xl:w-1/3">
+                    <div class="mb-10 overflow-hidden rounded-lg bg-white shadow-xl">
+                        <img
+                            src="/images/AR2_0265.jpg"
+                            alt="image"
+                            class="w-full"
+                        />
+                        <div class="p-2 text-center sm:p-2 md:p-2 xl:p-4">
+                            <p class="text-body-color  text-base hover:leading-relaxed">
+                            <!-- Lorem -->
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- 1.2 card -->
+                <div class="w-full px-4 md:w-1/2 xl:w-1/3">
+                    <div class="mb-10 overflow-hidden rounded-lg bg-white shadow-xl">
+                        <img
+                            src="/images/AR2_0272.jpg"
+                            alt="image"
+                            class="w-full"
+                        />
+                        <div class="p-2 text-center sm:p-2 md:p-2 xl:p-4">
+                            <p class="text-body-color  text-base hover:leading-relaxed">
+                            <!-- Lorem -->
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- 1.3 card -->
+                
+                
+            </div>
+        </div>
+    </section>
+    <!-- ====== Cards Section End -->
+
+
+</section>
+<hr style="border: 0; height: 2px; background: #333; background-image: linear-gradient(to right, #ccc, #333, #ccc);">
 </div>
