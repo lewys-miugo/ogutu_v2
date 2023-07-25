8fe7ab75cbe98cb765d4573e944ca952ecd1be78
lewys-miugo <lewiswambugu01@gmail.com>
2023-07-25 17:12:39 +0300

    Adding summernote and edit logic

--- app/Http/Livewire/Admin/AdminEditHomeBioComponent.php
+++ app/Http/Livewire/Admin/AdminEditHomeBioComponent.php
@@ -3,11 +3,34 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\HomeBio;
 
 class AdminEditHomeBioComponent extends Component
 {
+    public $homeBio;
+    public $home_bio=[];
+
+    public function mount()
+    {
+        $homeBio =HomeBio::first();
+        $this->home_bio=$homeBio->home_bio;
+
+    }
+
+    public function updateHomeBio()
+    {
+        $homeBio=HomeBio::first();
+
+        $homeBio->home_bio=$this->home_bio;
+        $homeBio->save();
+        session()->flash('message','Home biography has been updated');
+
+        return redirect()->route('admin.home.bio');
+    }
     public function render()
     {
+        // $homeBio=HomeBio::first();,['homeBio'=>$homeBio]
+
         return view('livewire.admin.admin-edit-home-bio-component');
     }
 }
--- resources/views/livewire/admin/admin-edit-home-bio-component.blade.php
+++ resources/views/livewire/admin/admin-edit-home-bio-component.blade.php
@@ -1,3 +1,79 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    {{-- Success is as dangerous as failure. --}}
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <div class="w-96 px-4 py-8">
+        <div class="w-full mx-auto bg-white rounded-lg overflow-hidden shadow-md">
+            <div class="py-4 px-6">
+                <!-- <h2 class="text-2xl font-bold mb-4">return Policy</h2> -->
+                <form wire:submit.prevent="updateHomeBio">
+                    <label class="text-2xl text-center font-bold mb-4" for="home_bio">Home Biography edit</label>
+
+                    <div class="mb-4">
+                        <!-- <h2 class="text-center underline underline-offset-2">Preview</h2>
+                        <p class="mx-8">{!!$home_bio!!}</p> -->
+                        <p>color: #00063d and size: heading 3</p>
+                    </div>
+
+                    <div wire:ignore class="w-full h-full">
+                        <textarea name="home_bio" id="home_bio" wire:model="home_bio" class="w-full h-full">
+                            {!! $home_bio !!}
+                        </textarea>
+                        
+                    </div>
+
+                    <!-- <div>
+                    dd({{-- $homeBio->home_bio --}})
+                    </div> -->
+                    
+
+                    @error('home_bio')
+                        <p class="text-white bg-red-300">{{$message}}</p>
+                    @enderror
+
+                    <div class="flex justify-center mt-2">
+                        <button type="submit" class="flex bg-editlink rounded-lg m-4 p-2 w-fit">Update</button>
+                    </div>
+
+                    @if(Session::has('message'))
+                        <div class="flex items-center justify-center bg-green-200 text-green-700 p-4 rounded-lg mt-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                            <span class="font-semibold">Success! | {{Session::get('message')}}</span>
+                        </div>
+                    @endif
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- <script>
+      $('#home_bio').summernote({
+        // placeholder: 'Hello Bootstrap 4',
+        tabsize: 2,
+        height: 100,
+        callbacks: {
+                onChange: function(contents, $editable) {
+                    @this.set('home_bio',contents);
+                }
+            }
+      });
+    </script> -->
+
+    <script>
+        $('#home_bio').summernote({
+            placeholder: 'Hello Bootstrap 5',
+            tabsize: 4,
+            height: 300,
+            width:1000,
+            callbacks: {
+                onChange: function(contents, $editable) {
+                    @this.set('home_bio',contents);
+                }
+            }
+
+        });
+        {{-- $('#home_bio').summernote('code', '{{$homeBio->home_bio}}'); --}}
+    </script> 
+    
 </div>
